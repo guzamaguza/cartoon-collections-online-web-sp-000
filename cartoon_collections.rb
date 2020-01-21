4767a124622a3a0296782310b0a66a67ff56e4d5
Mark A Guza <guzamaguza@gmail.com>
2020-01-21 06:25:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|word| word == chesse_types}
+  array.find {|word| word == cheese_types}
 end
