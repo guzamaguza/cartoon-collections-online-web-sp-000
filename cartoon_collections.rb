2cff7c84c34cf0401764636462985cb25c9348f3
Mark A Guza <guzamaguza@gmail.com>
2020-01-21 06:27:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|word| cheese_types}
+  array.find {|word| cheese_types.include? word}
 end
