16eedcf7b63a30dbd8af22f3cf460d14204c081d
Mark A Guza <guzamaguza@gmail.com>
2020-01-21 06:20:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls
-
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4}
 end
 
 def find_the_cheese
