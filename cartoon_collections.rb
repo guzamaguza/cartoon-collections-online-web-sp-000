bfddf8f14bffc9eb761829478d6a736d56255806
Mark A Guza <guzamaguza@gmail.com>
2020-01-21 06:23:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include? {|word| chesse_types}
 end
