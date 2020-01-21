3ea39bf0abfedf68f373f91696711087d74002af
Mark A Guza <guzamaguza@gmail.com>
2020-01-21 05:56:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
+  planeteer_calls.each do |call|
       puts " \'#{call.capitalize}!\' "
   end
 end
